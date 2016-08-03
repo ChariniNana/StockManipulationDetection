d22392bdb8f32b4c4a6ab4d71dd49aeb4ed2d222
Yasith Lokuge <yasith1@gmail.com>
2016-08-03 10:27:55 +0530

    loggers changed to slf4j

--- src/main/java/org/wso2/cseToolkit/Client.java
+++ src/main/java/org/wso2/cseToolkit/Client.java
@@ -18,13 +18,6 @@
 
 package org.wso2.cseToolkit;
 
-import org.apache.commons.io.FilenameUtils;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.apache.log4j.BasicConfigurator;
-import org.wso2.carbon.databridge.agent.AgentHolder;
-import org.wso2.carbon.databridge.agent.DataPublisher;
-import org.wso2.carbon.databridge.commons.Event;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -38,10 +31,16 @@ import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
-import java.util.stream.Collectors;
+
+import org.apache.commons.io.FilenameUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.wso2.carbon.databridge.agent.AgentHolder;
+import org.wso2.carbon.databridge.agent.DataPublisher;
+import org.wso2.carbon.databridge.commons.Event;
 
 public class Client {
-    private static Log log = LogFactory.getLog(Client.class);
+    private static Logger log = LoggerFactory.getLogger(Client.class);
     private static final SimpleDateFormat sdf = new SimpleDateFormat("dd-MMM-yy hh:mm:ss a", Locale.ENGLISH);
     private static final SimpleDateFormat sdfDate = new SimpleDateFormat("dd-MMM-yy", Locale.ENGLISH);
     private static Date date;
@@ -57,8 +56,7 @@ public class Client {
     private static int count = 1; // TODO: 8/2/16 make this 0. recheck
 
     
-    public static void main(String[] args) {
-    	BasicConfigurator.configure(); 
+    public static void main(String[] args) {    	
         log.info(Arrays.deepToString(args));
         try {
             AgentHolder.setConfigPath(DataPublisherUtil.getDataAgentConfigPath());
